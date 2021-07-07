168092dbb6920bea9ef7f8ad0f6152113d751b50
BrunoYokoyama <byokoyama1603@gmail.com>
2021-07-07 03:58:23 -0500

    Pantalla de creditos

---
+++ Assets/Scripts/Interface/Creditos.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+    public GameObject panel;
+    public bool end;
+    public float initialVolume;
+    public AudioSource ad;
+    void Start()
+    {
+        end = false;
+        initialVolume = PlayerPrefs.GetFloat("volume");
+        initialVolume *= ad.volume;
+        ad.volume = initialVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VolumeChange();
+        
+    }
+
+    public void ActivatePanel()
+    {
+        panel.SetActive(true);
+    }
+    public void SetEnd()
+    {
+        end = true;
+    }
+    void VolumeChange()
+    {
+        if (end)
+        {
+            ad.volume -=Time.deltaTime/18;
+            Debug.Log(ad.volume);
+        }
+    }
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
