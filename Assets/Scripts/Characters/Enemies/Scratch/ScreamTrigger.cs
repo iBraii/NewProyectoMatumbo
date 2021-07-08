e3f17200ace4cb80dad7db8688081e09ded56ca9
Adrian Blume <adrianblume1005@gmail.com>
2021-07-08 01:30:53 -0500

    RankingTable

--- Assets/Scripts/Characters/Enemies/Scratch/ScreamTrigger.cs
+++ Assets/Scripts/Characters/Enemies/Scratch/ScreamTrigger.cs
@@ -6,19 +6,18 @@ public class ScreamTrigger : MonoBehaviour
 {
     public string playerTag;
     private EnemiesAttack sc_enemiesAtk;
-    private ObjectM sc_ObjectM;
+    [SerializeField] private ObjectM sc_ObjectM;
     public int stressDamage;
     // Start is called before the first frame update
     void Start()
     {
         sc_enemiesAtk = GetComponent<EnemiesAttack>();
-        sc_ObjectM = GameObject.Find("Enemigo2").GetComponent<ObjectM>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(sc_ObjectM.blink == true && sc_ObjectM.isDenied == false)
+        if (other.gameObject.CompareTag(playerTag))
         {
-            if (other.gameObject.CompareTag(playerTag))
+            if (sc_ObjectM.blink == true && sc_ObjectM.isDenied == false)
             {
                 sc_enemiesAtk.SpectresAttack(stressDamage);
             }
@@ -26,9 +25,9 @@ public class ScreamTrigger : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
-        if (sc_ObjectM.blink == true && sc_ObjectM.isDenied == false)
+        if (other.gameObject.CompareTag(playerTag))
         {
-            if (other.gameObject.CompareTag(playerTag))
+            if (sc_ObjectM.blink == true && sc_ObjectM.isDenied == false)
             {
                 sc_enemiesAtk.SpectresAttack(stressDamage);
             }
--- Assets/Scripts/Interface/TransitionTrigger.cs
+++ Assets/Scripts/Interface/TransitionTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TransitionTrigger : MonoBehaviour
 {
@@ -8,7 +9,6 @@ public class TransitionTrigger : MonoBehaviour
     public string levelCompleted;
     public string levelName, defeatName;
     public float maxTimer;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +22,17 @@ public class TransitionTrigger : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        sc_scCh.Change(levelName);
+        UpdateData.ud.actualTotalTime += maxTimer;
+        if(SceneManager.GetActiveScene().name == "Level2")
+        {
+            UpdateData.ud.lastTotalTime = UpdateData.ud.actualTotalTime;
+            UpdateData.ud.SendLeaderBoard((int)UpdateData.ud.lastTotalTime);
+        }
         PlayerPrefs.SetInt(levelCompleted, 1);
+        sc_scCh.Change(levelName);    
     }
     public void TimeControl()
     {
         maxTimer += Time.deltaTime;
-        //if (maxTimer <= 0)
-        //{
-        //    sc_scCh.Change(defeatName);
-        //}
     }
 }
