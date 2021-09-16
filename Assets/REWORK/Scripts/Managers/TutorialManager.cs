1fbfd818b6a44a8b731bcbc15a79b2293ab6d562
BrunoYokoyama <byokoyama1603@gmail.com>
2021-09-15 21:53:44 -0500

    Nivel tutorial y nivel 4

--- Assets/REWORK/Scripts/Managers/TutorialManager.cs
+++ Assets/REWORK/Scripts/Managers/TutorialManager.cs
@@ -1,25 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
-
+using UnityEngine.InputSystem;
+using TMPro;
 public class TutorialManager : MonoBehaviour
 {
     public GameObject player;
     public GameObject firstEnemy;
-    void Start()
+
+    public TextMeshPro[] indication;
+
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction cameraAction;
+
+    private bool movement, look, jump;
+    private void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        cameraAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
     }
 
-    
     void Update()
     {
-        
+        GetPlayerInput();
     }
 
+    private void GetPlayerInput()
+    {
+        if (moveAction.triggered)
+            ChangeAlphaValue(indication[0], 20);
+        if (cameraAction.triggered)
+            ChangeAlphaValue(indication[1], .4f);
+        if (jumpAction.triggered)
+            ChangeAlphaValue(indication[2], 100);
+    }
 
-
+    private void ChangeAlphaValue(TextMeshPro text,float intensity)
+    {
+        text.color = new Color(255, 255, 255, text.color.a - (intensity*Time.deltaTime));
+    }
 
 
     private void OnTriggerEnter(Collider other)
