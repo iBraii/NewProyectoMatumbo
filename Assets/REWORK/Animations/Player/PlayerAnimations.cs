67518d7cc2b891ab265f74d2938944efdaf84d1a
Adrian Blume <adrianblume1005@gmail.com>
2021-10-06 21:37:23 -0500

    avance anims placeholder, por cambiar

--- Assets/REWORK/Animations/Player/PlayerAnimations.cs
+++ Assets/REWORK/Animations/Player/PlayerAnimations.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
-    private Animator anim;
+    [SerializeField] private Animator anim;
+    PlayerSingleton ps;
 
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
+        ps = PlayerSingleton.Instance;
     }
 
     private void Update()
     {
-
+        anim.SetBool("isMoving", ps.isMoving);
+        anim.SetBool("isHiding", ps.isHiding);
+        anim.SetBool("Dying", ps.stress >= 10);
+        anim.SetBool("Grabbing", GetComponent<GrabBox>().grabingBox);
     }
 
 }
